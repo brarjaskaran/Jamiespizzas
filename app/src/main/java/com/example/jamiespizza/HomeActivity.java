808f6589467d3fb0d9e9bb0632c115a0403c3487
brarjas <jaskaranbrar77@gmail.com>
2020-01-12 22:39:08 +1030

    Added navigation Drawer in the HomeActivity.
    1. Customer can also logout through the navigation drawer.

--- app/src/main/java/com/example/jamiespizza/HomeActivity.java
+++ app/src/main/java/com/example/jamiespizza/HomeActivity.java
@@ -1,39 +1,68 @@
 package com.example.jamiespizza;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import io.paperdb.Paper;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
-
-    private Button logout;
-
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        logout = findViewById(R.id.logout);
+        DrawerLayout drawerLayout;
+        Toolbar toolbar;
+        NavigationView navigationView;
+        ActionBarDrawerToggle toggle;
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Paper.book().destroy();
+            drawerLayout = findViewById(R.id.drawer);
+            toolbar = findViewById(R.id.toolbar);
+            navigationView = findViewById(R.id.navigationView);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawerOpen,R.string.drawerClose);
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+            navigationView.setNavigationItemSelectedListener(this);
 
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.profile:
+                Toast.makeText(HomeActivity.this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.contact:
+                Toast.makeText(HomeActivity.this, "Contact us Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:
+                Toast.makeText(HomeActivity.this, "About us Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
                 Intent intent = new Intent(HomeActivity.this, MainActivity.class);
                 startActivity(intent);
+                Toast.makeText(HomeActivity.this, "Logout Selected", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
 
-
-            }
-        });
-
-
+        return false;
     }
 }
