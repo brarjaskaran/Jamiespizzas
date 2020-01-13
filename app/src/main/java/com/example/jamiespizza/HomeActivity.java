5e5928859fd426040b29c6cff749ff9a10b472dd
brarjas <jaskaranbrar77@gmail.com>
2020-01-13 21:07:36 +1030

    Recyclar View is working Perfect

--- app/src/main/java/com/example/jamiespizza/HomeActivity.java
+++ app/src/main/java/com/example/jamiespizza/HomeActivity.java
@@ -5,18 +5,29 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.jamiespizza.Model.Products;
 import com.example.jamiespizza.Prevalent.Prevalent;
+import com.example.jamiespizza.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import io.paperdb.Paper;
@@ -29,6 +40,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
 
+    private DatabaseReference ProductRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
 
 
 
@@ -37,12 +52,16 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        ProductRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
         Paper.init(this);
 
 
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.my_toolbar);
         navigationView = findViewById(R.id.navigationView);
+        recyclerView = findViewById(R.id.recycler_menu);
+
 
 
         toolbar.setTitle("Home");
@@ -64,6 +83,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
 
 
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,13 +93,48 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
-
-
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        FirebaseRecyclerOptions<Products> options =
+                new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(ProductRef, Products.class).build();
+
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder productViewHolder, int i, @NonNull Products products) {
+
+                        productViewHolder.textProductName.setText(products.getPname());
+                        productViewHolder.textProductDescription.setText(products.getDescription());
+                        productViewHolder.textProductPrice.setText("Price = $ "+ products.getPrice());
+                        Picasso.get().load(products.getImage()).into(productViewHolder.imageView);
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item_layout, parent, false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
--- app/src/main/java/com/example/jamiespizza/ViewHolder/ProductViewHolder.java
+++ app/src/main/java/com/example/jamiespizza/ViewHolder/ProductViewHolder.java
@@ -7,11 +7,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.jamiespizza.Interface.ItemClickListner;
 import com.example.jamiespizza.R;
 
 public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    public TextView textProductName, textProductDescription;
+
+    public TextView textProductName, textProductDescription, textProductPrice;
     public ImageView imageView;
+    public ItemClickListner listner;
 
     public ProductViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -19,12 +22,18 @@ public class ProductViewHolder extends RecyclerView.ViewHolder implements View.O
         imageView = itemView.findViewById(R.id.product_image);
         textProductName = itemView.findViewById(R.id.product_name);
         textProductDescription = itemView.findViewById(R.id.product_description);
+        textProductPrice = itemView.findViewById(R.id.product_price);
 
 
     }
 
+    public void setItemClickListner(ItemClickListner listner){
+        this.listner = listner;
+    }
+
     @Override
     public void onClick(View v) {
+        listner.onClick(v, getAdapterPosition(), false);
 
     }
 }
